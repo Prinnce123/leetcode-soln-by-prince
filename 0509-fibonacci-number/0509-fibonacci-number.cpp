862c1948d009e922d75037e74c6dcca24f6a484f
Prinnce123 <110710535+Prinnce123@users.noreply.github.com>
2025-09-08 10:23:23 +0530

    Time: 2 ms (77.36%), Space: 7.9 MB (36.57%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int fib(int n) {
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return 1;
+        
+        int prev2 = 0;
+        int prev = 1;
+        int cur;
+        
+        for (int i = 2; i <= n; i++) {
+            cur = prev + prev2;
+            prev2 = prev;
+            prev = cur;
+        }
+        
+        return cur;
+    }
+};
